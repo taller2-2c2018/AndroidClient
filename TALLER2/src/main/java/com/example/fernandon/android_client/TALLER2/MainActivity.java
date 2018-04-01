41e20db810d6a82f8967eacb423c05d58bca3f8d
fernandonitz <nitzfernando@gmail.com>
2018-04-01 12:00:43 -0300

    V:1.0.6.0.
    Application Main - Adding layout for historys.

--- TALLER2/src/main/java/com/example/fernandon/android_client/TALLER2/MainActivity.java
+++ TALLER2/src/main/java/com/example/fernandon/android_client/TALLER2/MainActivity.java
@@ -189,7 +189,7 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
             if (getArguments().getInt(ARG_SECTION_NUMBER) == 1 ){
-                View rootView = inflater.inflate(R.layout.activity_primary, container, false);
+                View rootView = inflater.inflate(R.layout.fragment_main, container, false);
                 //TextView textView = (TextView) rootView.findViewById(R.id.section_label);
                 //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
                 return rootView;
